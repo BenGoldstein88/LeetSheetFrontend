6e2d3690c784f18e818fe698bacc662b77920004
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-11-14 16:32:51 -0600

    fixed new measure form to appropriately update state. does not yet communicate with backend

--- app/components/NewMeasureForm.js
+++ app/components/NewMeasureForm.js
@@ -5,7 +5,23 @@ export default class NewMeasureForm extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+    	chords: ['', '', '', '']
+    }
+
+    this.handleChange = this.handleChange.bind(this)
   }
+
+  handleChange(e) {
+  	var chord = e.target.value
+  	var arrayIndex = e.target.getAttribute("name")
+
+  	this.state.chords[arrayIndex] = chord
+  	console.log("State after change: ", this.state)
+  }
+
+
 
   render() {
     return (
@@ -15,6 +31,27 @@ export default class NewMeasureForm extends React.Component {
         border: "1px solid blue",
         display: "inline-block"
         }} >
+        <input type='text' name={0} onChange={this.handleChange} style={{
+    		height: "99%",
+    		width: "24%",
+    		display: "inline-block"
+        }}/>
+
+        <input type='text' onChange={this.handleChange} name={1} style={{
+    		height: "99%",
+    		width: "24%",
+    		display: "inline-block"
+        }}/>
+        <input type='text' onChange={this.handleChange} name={2} style={{
+    		height: "99%",
+    		width: "24%",
+    		display: "inline-block"
+        }}/>
+        <input type='text' onChange={this.handleChange} name={3} style={{
+    		height: "99%",
+    		width: "24%",
+    		display: "inline-block"
+        }}/>
         
 
 
